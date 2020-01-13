eec3a320ffee7e23997c52df5d366e90f9784c59
balabg94 <balagovind2094@gmail.com>
2020-01-13 16:31:30 +0530

    Test case for navigating east when in room 2

--- test_game.py
+++ test_game.py
@@ -10,3 +10,9 @@ def test_navigate_south():
     a = game.navigate(map,direction = 's',current_position = 1)
     e = 3
     assert a == e
+
+def test_navigate_east():
+    map = {1:{'n':2,'s':3,'e':0,'w':0,'room':'Hall'},2:{'n':0,'s':1,'e':3,'w':0,'room':'Kitchen'},3:{'n':1,'s':0,'e':0,'w':2,'room':'Garden'}}
+    a = game.navigate(map,direction = 'e',current_position = 2)
+    e = 3
+    assert a == e
