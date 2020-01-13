1eb588ef8ea6cd56ed4a1bbb87d114b31193eb25
balabg94 <balagovind2094@gmail.com>
2020-01-13 17:06:23 +0530

    Test case for where thereis no way from the current position

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 def navigate(map,direction,current_position):
     if map[current_position][direction] != 0:
         next_position = map[current_position][direction]
+    else:
+        next_position = current_position
     return next_position
--- test_game.py
+++ test_game.py
@@ -22,3 +22,9 @@ def test_navigate_west():
     a = game.navigate(map,direction = 'w',current_position = 3)
     e = 2
     assert a == e
+
+def test_navigate_no_where():
+    map = {1:{'n':2,'s':3,'e':0,'w':0,'room':'Hall'},2:{'n':0,'s':1,'e':3,'w':0,'room':'Kitchen'},3:{'n':1,'s':0,'e':0,'w':2,'room':'Garden'}}
+    a = game.navigate(map,direction = 'w',current_position = 2)
+    e = 2
+    assert a == e
