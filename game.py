89933ca4e168d1bec53b08ba9042ccb16f2cd091
balabg94 <balagovind2094@gmail.com>
2020-01-14 11:49:35 +0530

    Basic Game

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+import sys
+import ast
+
 def navigate(map,direction,current_position):
     if map[current_position][direction] != 0:
         next_position = map[current_position][direction]
@@ -7,13 +10,25 @@ def navigate(map,direction,current_position):
 
 def main():
     print("Start")
+    print("Type 'n' for NORTH")
+    print("Type 's' for SOUTH")
+    print("Type 'w' for WEST")
+    print("Type 'e' for EAST")
+    print()
+    print("You are in the hall now")
+    print()
     
-    map = {1:{'n':2,'s':3,'e':0,'w':0,'room':'Hall'},2:{'n':0,'s':1,'e':3,'w':0,'room':'Kitchen'},3:{'n':1,'s':0,'e':0,'w':2,'room':'Garden'}}
+    sys.argv.pop(0)
+    
+    with open(sys.argv[0]) as f:
+        map = ast.literal_eval(f.read())
     
     d = ""
     current_position = 1
-    while d != "exit"  :
+    while True  :
         d = input(">>>>> ")
+        if d == 'exit':
+            break
         next_position = navigate(map,d,current_position)
         
         if current_position == next_position:
@@ -22,5 +37,5 @@ def main():
         
         print("You are in {}".format(map[next_position]['room']))
         current_position = next_position
-
+    
 main()
